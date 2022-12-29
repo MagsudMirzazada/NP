2e779056790b5a8d0b81f635ebaead6d21fbc478
MagsudMirzazada <magsud.mirzezade@gmail.com>
2022-12-29 09:28:34 +0400

    Update client.py

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import threading
 import socket
 alias = input('Choose an alias >>> ')
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 59000))
+client.connect(('127.0.0.1', 5555))
 
 
 def client_receive():
